d0d668a6fed830b4f983da1452a9341d11ed82c9
Alexia <Alexia@Alexia-Windows>
2020-11-15 06:15:21 -0300

    Primer commit

---
+++ WebPolideportivo/src/main/java/com/polideportivo/WebPolideportivoApplication.java
@@ -0,0 +1,13 @@
+package com.polideportivo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebPolideportivoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebPolideportivoApplication.class, args);
+	}
+
+}
---
+++ WebPolideportivo/src/test/java/com/polideportivo/WebPolideportivoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polideportivo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebPolideportivoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
